c10927b9fb6dd3bb0dd5bb46fcc3f792bcaf2840
1192801 <himanshusogarwal@gmail.com>
2023-08-30 10:25:42 +0530

    Created all the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './home-page/carousel/carousel.component';
+import { OfferingsComponent } from './home-page/offerings/offerings.component';
+import { BlogsComponent } from './home-page/blogs/blogs.component';
+import { AnnouncementsComponent } from './home-page/announcements/announcements.component';
+import { DealsComponent } from './home-page/deals/deals.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent,
+    FooterComponent,
+    CarouselComponent,
+    OfferingsComponent,
+    BlogsComponent,
+    AnnouncementsComponent,
+    DealsComponent
   ],
   imports: [
     BrowserModule,
